c72bc8d161dba9a02edbf072ec31a19bee959c6e
Vanilla <solmarians05@gmail.com>
2020-05-26 20:41:57 +0300

    Updates

---
+++ precache-manifest.718fe6cb4f23a918e5edd0147979b78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2815b9074b128503f8d050080e0ae175",
+    "url": "/News-app/index.html"
+  },
+  {
+    "revision": "52a79cafd017662b1f40",
+    "url": "/News-app/static/css/2.09cf91a0.chunk.css"
+  },
+  {
+    "revision": "d3718606fc40f36a8f69",
+    "url": "/News-app/static/css/main.e0c8923a.chunk.css"
+  },
+  {
+    "revision": "52a79cafd017662b1f40",
+    "url": "/News-app/static/js/2.62089f21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/News-app/static/js/2.62089f21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3718606fc40f36a8f69",
+    "url": "/News-app/static/js/main.6dec7a78.chunk.js"
+  },
+  {
+    "revision": "283a6bda53fcc5749b01",
+    "url": "/News-app/static/js/runtime-main.8b2e45d7.js"
+  }
+]);
